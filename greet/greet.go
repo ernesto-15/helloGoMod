ed77eeaa374ce1cbf867d0d3441ce0bb6edf5de2
Ernesto Angulo <ernestoangulojimenez@gmail.com>
2020-09-24 15:27:49 -0500

    first commit

---
+++ greet/greet.go
@@ -0,0 +1,13 @@
+package greet
+
+var emoji = "💚"
+
+//English returns a greet in english
+func English() string {
+	return "Hi" + emoji
+}
+
+//Italian returns a greet in italian
+func Italian() string {
+	return "Ciao" + emoji
+}
---
+++ greet/spanish.go
@@ -0,0 +1,6 @@
+package greet
+
+//Spanish returns a greet in spanish
+func Spanish() string  {
+	return "Hola" + emoji
+}
